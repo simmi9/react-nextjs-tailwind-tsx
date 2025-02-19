90ec7d9709b47a0f1a9e0d803c151360940b7822
Bhawna Keswani <bhawna.keswani20@gmail.com>
2025-02-19 13:46:53 -0500

    refactoring

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,13 @@ import React from 'react'
 import { Movie } from './types/Movie'
 import MovieResults from '@/components/MovieResults'
 type Props={
-  searchParams:{ [key: string]: string | string[] | undefined }
+  searchParams:Promise<{ [key: string]: string | string[] | undefined }>
 }
 
 export default async function Home({searchParams}:Props) {
   const  API_KEY= process.env.REACT_APP_API_KEY
-  const genre= searchParams.genre||'fetchTrending'
+  const param=await searchParams;
+  const genre=  param.genre||'fetchTrending'
     const res= await fetch(`https://api.themoviedb.org/3${ genre==='fetchTopRated'?`/movie/top_rated`:`/trending/all/week`}?api_key=${API_KEY}`)
     const data= await res.json();
     if(!res.ok){
