f3ba60ef959143940c498f38acd552187c442dfc
Bhawna Keswani <bhawna.keswani20@gmail.com>
2025-02-19 12:34:43 -0500

    refactoring

--- app/Providers.tsx
+++ app/Providers.tsx
@@ -1,8 +1,11 @@
 'use client'
-import React from 'react'
+import React, { JSX } from 'react'
 import {ThemeProvider} from 'next-themes'
 
-export default function Providers({children}:any) {
+type Props = {
+  children: string | JSX.Element | JSX.Element[] 
+}
+export default function Providers({children}:Props) {
   return (
  <ThemeProvider defaultTheme='system' attribute='class'>
     <div  className='text-gray-700 dark:text-gray-200 dark: bg-gray-700 min-h-screen select-none transition-color duration-300s'>
