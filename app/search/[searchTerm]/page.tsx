fb7ba3ed36dccad4ff4ac4d4b88743a0a828a216
Bhawna Keswani <bhawna.keswani20@gmail.com>
2025-02-19 13:36:53 -0500

    refactoring

--- app/search/[searchTerm]/page.tsx
+++ app/search/[searchTerm]/page.tsx
@@ -2,13 +2,13 @@
 import MovieResults from '@/components/MovieResults';
 import React from 'react'
 type Props={
-  searchTerm:string
+  searchParams:{ genre:string, searchTerm:string}
 }
-export default  async function  SearchPage({searchTerm}:Props) {
+export default  async function  SearchPage({searchParams}:Props) {
 
     const  API_KEY= process.env.REACT_APP_API_KEY;
 
- const res= await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${searchTerm}&language=en-US&page=1&include_adult=false`)
+ const res= await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${searchParams.searchTerm}&language=en-US&page=1&include_adult=false`)
 const data= await res.json()
 if(!res.ok){
     throw Error;
